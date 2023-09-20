c7adaf2aefe9957c410cb5e2be2f36191abe9ff2
nguyenanhung <info@nguyenanhung.com>
2023-09-20 23:21:49 +0700

    Update Project at Wed Sep 20 23:21:49 +07 2023

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -17,9 +17,45 @@ if (!function_exists('smart_bear_cms_cdn_url_query_removed')) {
         return \nguyenanhung\Platforms\SmartBearCMS\Library\ExternalCDNOptimize\ExternalCDNOptimize::urlQueryRemoved($url);
     }
 }
+if (!function_exists('smart_bear_cms_external_cdn_work_with_query_resize_list')) {
+    function smart_bear_cms_external_cdn_work_with_query_resize_list($url = '', $default = 'front_small')
+    {
+        return \nguyenanhung\Platforms\SmartBearCMS\Library\ExternalCDNOptimize\ExternalCDNOptimize::externalCdnWorkWithUrlQueryResize();
+    }
+}
+if (!function_exists('smart_bear_cms_external_cdn_vov_vn_resize')) {
+    function smart_bear_cms_external_cdn_vov_vn_resize($url = '', $default = 'front_small')
+    {
+        return \nguyenanhung\Platforms\SmartBearCMS\Library\ExternalCDNOptimize\ExternalCDNOptimize::externalVovCdnResize($url, $default);
+    }
+}
 if (!function_exists('smart_bear_cms_external_cdn_url_optimize')) {
     function smart_bear_cms_external_cdn_url_optimize($url = '', $width = 345, $height = 200)
     {
         return \nguyenanhung\Platforms\SmartBearCMS\Library\ExternalCDNOptimize\ExternalCDNOptimize::externalCdnOptimize($url, $width, $height);
+    }
+}
+if (!function_exists('smart_bear_cms_external_cdn_url_optimize_epi_cms')) {
+    function smart_bear_cms_external_cdn_url_optimize_epi_cms($url = '', $width = 345, $height = 200)
+    {
+        return \nguyenanhung\Platforms\SmartBearCMS\Library\ExternalCDNOptimize\ExternalCDNOptimize::externalCdnEPICMSOptimize($url, $width, $height);
+    }
+}
+if (!function_exists('smart_bear_cms_external_cdn_url_optimize_vccorp_cms')) {
+    function smart_bear_cms_external_cdn_url_optimize_vccorp_cms($url = '', $width = 345, $height = 200)
+    {
+        return \nguyenanhung\Platforms\SmartBearCMS\Library\ExternalCDNOptimize\ExternalCDNOptimize::externalCdnVCCorpCMSOptimize($url, $width, $height);
+    }
+}
+if (!function_exists('smart_bear_cms_external_cdn_url_optimize_one_cms')) {
+    function smart_bear_cms_external_cdn_url_optimize_one_cms($url = '', $width = 345, $height = 200)
+    {
+        return \nguyenanhung\Platforms\SmartBearCMS\Library\ExternalCDNOptimize\ExternalCDNOptimize::externalCdnOneCMSOptimize($url, $width, $height);
+    }
+}
+if (!function_exists('smart_bear_cms_external_cdn_url_optimize_netlink_cms')) {
+    function smart_bear_cms_external_cdn_url_optimize_netlink_cms($url = '', $width = 345, $height = 200)
+    {
+        return \nguyenanhung\Platforms\SmartBearCMS\Library\ExternalCDNOptimize\ExternalCDNOptimize::externalCdnNetLinkCMSOptimize($url, $width, $height);
     }
 }
