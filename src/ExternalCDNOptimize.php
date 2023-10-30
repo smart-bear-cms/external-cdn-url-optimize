017d4c7abba32889778cc0fa8422afef88db605a
nguyenanhung <info@nguyenanhung.com>
2023-10-30 15:53:24 +0700

    Update Project at Mon Oct 30 15:53:24 +07 2023

--- src/ExternalCDNOptimize.php
+++ src/ExternalCDNOptimize.php
@@ -447,6 +447,7 @@ class ExternalCDNOptimize
         $url = self::optimizeOneDomainCdnVCCorpCMSOptimize('https://suckhoedoisong.qltns.mediacdn.vn', $url, $width);
         $url = self::optimizeOneDomainCdnVCCorpCMSOptimize('https://congdankhuyenhoc.qltns.mediacdn.vn', $url, $width);
         $url = self::optimizeOneDomainCdnVCCorpCMSOptimize('https://tapchigiaothong.qltns.mediacdn.vn', $url, $width);
+        $url = self::optimizeOneDomainCdnVCCorpCMSOptimize('https://truyenhinhvov.qltns.mediacdn.vn', $url, $width);
         $url = self::optimizeOneDomainCdnVCCorpCMSOptimize('https://phapluatbandoc.mediacdn.vn', $url, $width);
         $url = self::optimizeOneDomainCdnVCCorpCMSOptimize('https://giadinh.mediacdn.vn', $url, $width);
         $url = self::optimizeOneDomainCdnVCCorpCMSOptimize('https://bcp.cdnchinhphu.vn', $url, $width);
