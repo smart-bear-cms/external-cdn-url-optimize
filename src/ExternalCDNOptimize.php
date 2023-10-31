85563a9a0ffe53485a8fef0185bb0389e6ce96ec
nguyenanhung <info@nguyenanhung.com>
2023-10-31 10:43:52 +0700

    Update Project at Tue Oct 31 10:43:52 +07 2023

--- src/ExternalCDNOptimize.php
+++ src/ExternalCDNOptimize.php
@@ -510,6 +510,7 @@ class ExternalCDNOptimize
         $url = self::optimizeOneDomainCdnVCCorpCMSOptimize('https://bcp.cdnchinhphu.vn', $url, $width);
         $url = self::optimizeOneDomainCdnVCCorpCMSOptimize('https://tl.cdnchinhphu.vn', $url, $width);
         $url = self::optimizeOneDomainCdnVCCorpCMSOptimize('https://tphcm.cdnchinhphu.vn', $url, $width);
+        $url = self::optimizeOneDomainCdnVCCorpCMSOptimize('https://xdcs.cdnchinhphu.vn', $url, $width);
         $url = self::optimizeOneDomainCdnVCCorpCMSOptimize('https://dntt.mediacdn.vn', $url, $width);
         $url = self::optimizeOneDomainCdnVCCorpCMSOptimize('https://sport5.mediacdn.vn', $url, $width);
         $url = self::optimizeOneDomainCdnVCCorpCMSOptimize('https://phunuso.mediacdn.vn', $url, $width);
